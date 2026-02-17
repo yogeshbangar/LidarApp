f88e7d15bbec5ae5f2aa9497ddb583140319ce2e
yogeshbangar <yogeshbangar@gmail.com>
2026-02-17 11:21:19 +0400

    Update User.js

--- node/user.js
+++ node/user.js
@@ -8,6 +8,13 @@ const userSchema = new mongoose.Schema(
       name: { type: String, required: true },
       email: { type: String, required: true, unique: true },
       age: { type: Number },
+      token: { type: String },
+      refreshToken: { type: String },
+      password: { type: String },
+      role: { type: String, enum: ["admin", "user"], default: "user" },
+      status: { type: String, enum: ["active", "inactive"], default: "active" },
+      createdAt: { type: Date, default: Date.now },
+      updatedAt: { type: Date, default: Date.now },
     },
     { timestamps: true }
   );
